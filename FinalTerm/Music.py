17e68c5949292aead85796e21c16d750dd7f43a6
OnlyFall <kwb010712@naver.com>
2018-11-03 21:45:20 +0900

    음악삽입을 위한 클래스 구현

--- FinalTerm/Music.py
+++ FinalTerm/Music.py
@@ -1,14 +1,29 @@
 from pico2d import *
 
 class Music:
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        self.music = data
 
-    def update(self):
-        pass
+    def rapeat_play(self):
+        Mix_PlayMusic(self.music, -1)
 
-    def crashCount(self, crash):
-        pass
+    def play(self, n=1):
+        Mix_PlayMusic(self.music, n)
 
-    def draw(self):
-        pass
+    def set_volume(self, v):
+        Mix_Volume(v)
+
+    def get_volume(self):
+        return Mix_VolumeMusic(-1)
+
+    def stop(self):
+        Mix_HaltMusic()
+
+    def pause(self):
+        Mix_PauseMusic()
+
+    def resume(self):
+        Mix_ResumeMusic()
+
+    def __del__(self):
+        Mix_FreeMusic(self.music)
