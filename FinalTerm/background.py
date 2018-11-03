04cfc6af0e1c662c8e411548d540ab39d4783dea
OnlyFall <kwb010712@naver.com>
2018-11-03 15:45:43 +0900

    패턴에 따른 뒷배경 사전로드

--- FinalTerm/background.py
+++ FinalTerm/background.py
@@ -2,13 +2,17 @@ from pico2d import *
 
 class Back:
     def __init__(self):
-        self.image = load_image('BG\\Stage1Map1.bmp')
-        self.step = load_image('STEP\\PT_0005.png')
+        self.stage1 = load_image('BG\\Stage1Map1.bmp')
+        self.step1 = load_image('STEP\\PT_0005.png')
+        self.step2 = load_image('STEP\\PT_0004.png')
+        self.step3 = load_image('STEP\\PT_0003.png')
+        self.step4 = load_image('STEP\\PT_0002.png')
+        self.step5 = load_image('STEP\\PT_0001.png')
 
     def update(self):
         pass
 
     def draw(self):
-        self.image.clip_draw(0, 0, 1600, 800, 1600 // 2, 800 // 2)
-        self.step.clip_draw(100, 50, 1600, 800, 1600 // 2, 800 // 2 - 50)
+        self.stage1.clip_draw(0, 0, 1600, 800, 1600 // 2, 800 // 2)
+        self.step1.clip_draw(100, 50, 1600, 800, 1600 // 2, 800 // 2 - 50)
         #self.image.draw(1200, 30)
