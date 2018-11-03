b427d42386a54c36d4fe7d401543a016152a7929
OnlyFall <kwb010712@naver.com>
2018-11-03 21:40:46 +0900

    BGM 추가를 위한 공간

---
+++ FinalTerm/Music.py
@@ -0,0 +1,14 @@
+from pico2d import *
+
+class Music:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def crashCount(self, crash):
+        pass
+
+    def draw(self):
+        pass
