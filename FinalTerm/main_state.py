3996911b047a7fc538f52a50800af5f6802ac6dd
OnlyFall <kwb010712@naver.com>
2018-11-03 23:47:43 +0900

    번갈아가면서 나오는 부채꼴 패턴
    사이에 비는 공간에도 들어감
    - 무적위치 방지

--- FinalTerm/main_state.py
+++ FinalTerm/main_state.py
@@ -71,10 +71,15 @@ def update():
 
     if get_time() - shootingTime > 1:
         shootingTime = get_time()
+        switch = random.randint(1,2)
 
         for i in range(10):
-            goX = math.cos((3.141592 / 180) * (i * 9) + 110) * 400 + 1580
-            goY = math.sin((3.141592 / 180) * (i * 9) + 110) * 400 + 750
+            if switch == 1:
+                goX = math.cos((3.141592 / 180) * (i * 9 + 180)) * 400 + 1580
+                goY = math.sin((3.141592 / 180) * (i * 9 + 180)) * 400 + 750
+            else:
+                goX = math.cos((3.141592 / 180) * (i * 9 + 176 )) * 400 + 1580
+                goY = math.sin((3.141592 / 180) * (i * 9 + 176)) * 400 + 750
             lengthX = goX - 1600
             lengthY = goY - 800
 
