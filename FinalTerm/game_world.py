0874dc3de23751ef7cb5c94d65ea51e3baa1f099
OnlyFall <kwb010712@naver.com>
2018-11-04 21:10:49 +0900

    game_world 함수추가
    -충돌 확인을 위한 함수
    -탄환을 꺼내기위한 레이어 한개만 꺼내오는 오브젝트 함수

--- FinalTerm/game_world.py
+++ FinalTerm/game_world.py
@@ -26,9 +26,20 @@ def clear():
     objects = [[], [], []]
 
 
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
 
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
 
+    return True
 
+def check_object(layer):
+    for i in objects[layer]:
+        yield i
 
 def all_objects():
     for i in range(len(objects)):
