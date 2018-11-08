02e00f41839480ec837835cd2bb811aa46ce5c93
OnlyFall <kwb010712@naver.com>
2018-11-08 14:49:06 +0900

    결과창 출력을 위한 스테이트 추가

---
+++ FinalTerm/ending_state.py
@@ -0,0 +1,57 @@
+import random
+import title_state
+import math
+import json
+import os
+from Shoot import Shoot
+from pico2d import *
+import game_framework
+import game_world
+import ShootPatern
+
+from Health import Health
+from Banana import Banana
+from background import Back
+
+name = "EndingState"
+
+
+def enter():
+    pass
+
+
+
+def exit():
+    pass
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                game_framework.quit()
+
+
+def update():
+    pass
+
+
+
+
+def draw():
+    pass
+
+
+
+
+
